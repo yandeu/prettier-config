b4f14233e07942c8c11a5b146c7b7faf18c52da3
Yannick <20306025+yandeu@users.noreply.github.com>
2021-11-24 19:00:49 +0100

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -2,7 +2,6 @@ module.exports = {
   $schema: 'http://json.schemastore.org/prettierrc',
   arrowParens: 'avoid',
   endOfLine: 'auto',
-  jsxBracketSameLine: true,
   printWidth: 120,
   semi: false,
   singleQuote: true,
